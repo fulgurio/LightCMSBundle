40b7dbfb2c15bdc69cc2dbcd4c0dffdb920f4218
Vincent GUERARD <v.guerard@fulgurio.net>
2013-01-18 22:07:50 +0100

    Correction on homepage meta on fixture

--- DataFixtures/ORM/LoadHomePage.php
+++ DataFixtures/ORM/LoadHomePage.php
@@ -22,9 +22,9 @@ class LoadPageData extends AbstractFixture implements FixtureInterface, OrderedF
         $homepage->setFullpath('');
         $homepage->setPosition(1);
         $homepage->setStatus('published');
-        $meta1 = $this->addMeta($homepage, 'keywords', 'lightcms,fulgurio,content management system');
+        $meta1 = $this->addMeta($homepage, 'meta_keywords', 'lightcms,fulgurio,content management system');
         $manager->persist($meta1);
-        $meta2 = $this->addMeta($homepage, 'description', 'Light content management system, using Symfony 2 framework');
+        $meta2 = $this->addMeta($homepage, 'meta_description', 'Light content management system, using Symfony 2 framework');
         $manager->persist($meta2);
         $homepage->addPageMeta($meta1);
         $homepage->addPageMeta($meta2);
