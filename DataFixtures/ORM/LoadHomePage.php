8e1cf65468ab91a4d6d94755e4aa3f8cb0d68ab1
Vincent <v.guerard@fulgurio.net>
2013-07-25 00:27:01 +0200

    Just correction on comment header of fixtures ...

--- DataFixtures/ORM/LoadHomePage.php
+++ DataFixtures/ORM/LoadHomePage.php
@@ -1,12 +1,12 @@
 <?php
 /*
  * This file is part of the LightCMSBundle package.
-*
-* (c) Fulgurio <http://fulgurio.net>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+ *
+ * (c) Fulgurio <http://fulgurio.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Fulgurio\LightCMSBundle\DataFixtures\ORM;
 
