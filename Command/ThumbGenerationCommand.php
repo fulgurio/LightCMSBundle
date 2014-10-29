5cf4cc9cad1634299a9c74c903c39376f84c05dd
fulgurio <v.guerard@fulgurio.net>
2014-10-29 20:37:00 +0100

    One thumb generation, a check is made if file doesn t exist

--- Command/ThumbGenerationCommand.php
+++ Command/ThumbGenerationCommand.php
@@ -1,13 +1,19 @@
 <?php
+/*
+ * This file is part of the LightCMSBundle package.
+ *
+ * (c) Fulgurio <http://fulgurio.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Fulgurio\LightCMSBundle\Command;
 
-//use Fulgurio\LightCMSBundle\Entity\User;
 use Fulgurio\LightCMSBundle\Utils\LightCMSUtils;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-//use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ThumbGenerationCommand extends ContainerAwareCommand
@@ -33,7 +39,7 @@ class ThumbGenerationCommand extends ContainerAwareCommand
     {
         $format = $input->getArgument('format');
         $medias = $this->getContainer()->get('doctrine')->getRepository('FulgurioLightCMSBundle:Media')->findAll();
-        $finfo = finfo_open(FILEINFO_MIME_TYPE); // return mime type ala mimetype extension
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
         $availableFormats = $this->getContainer()->getParameter('fulgurio_light_cms.thumbs');
         $uploadFullname = LightCMSUtils::getUploadDir();
         $uploadDirLen = strlen(LightCMSUtils::getUploadUrl());
@@ -43,23 +49,29 @@ class ThumbGenerationCommand extends ContainerAwareCommand
             if ($filename != '')
             {
                 $filename = substr($filename, $uploadDirLen);
-                $mimeType = finfo_file($finfo, $uploadFullname . $filename) . "\n";
-                if (substr($mimeType, 0, 5) == 'image') {
-                    foreach ($availableFormats as $formatName => $size) {
-						if ($format === NULL || $format == $formatName)
-						{
-                            $newFilename = LightCMSUtils::getThumbFilename($filename, $mimeType, $size);
-                            if (!file_exists($uploadFullname . $newFilename)) {
-                                echo '# Generation of ', $newFilename, ' (', $filename, ')', PHP_EOL;
-                                LightCMSUtils::cropPicture(
-                                    LightCMSUtils::getUploadDir() . $filename,
-                                    LightCMSUtils::getUploadDir() . LightCMSUtils::getThumbFilename($filename, $mimeType, $size),
-                                    $size['width'],
-                                    $size['height']
-                                );
+                if (file_exists($uploadFullname . $filename))
+                {
+                    $mimeType = finfo_file($finfo, $uploadFullname . $filename) . "\n";
+                    if (substr($mimeType, 0, 5) == 'image')
+                    {
+                        foreach ($availableFormats as $formatName => $size)
+                        {
+                            if ($format === NULL || $format == $formatName)
+                            {
+                                $newFilename = LightCMSUtils::getThumbFilename($filename, $mimeType, $size);
+                                if (!file_exists($uploadFullname . $newFilename))
+                                {
+                                    echo '# Generation of ', $newFilename, ' (', $filename, ')', PHP_EOL;
+                                    LightCMSUtils::cropPicture(
+                                        LightCMSUtils::getUploadDir() . $filename,
+                                        LightCMSUtils::getUploadDir() . LightCMSUtils::getThumbFilename($filename, $mimeType, $size),
+                                        $size['width'],
+                                        $size['height']
+                                    );
+                                }
                             }
-                        }
-                   }
+                       }
+                    }
                 }
             }
         }
